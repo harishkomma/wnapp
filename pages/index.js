f825da4683fb8c422aa1a049efeaa38e41b7f991
harish <harish.komma@onebillsoftware.com>
2021-05-15 23:46:13 +0530

    added web designer list

--- pages/index.js
+++ pages/index.js
@@ -65,6 +65,30 @@ const projects = [
     pinned: true,
     bgColorClass: "bg-blue-600",
   },
+  {
+    id: 1,
+    title: "Web Designer",
+    initials: "GA",
+    team: "Front-end Developer",
+    members: [
+      {
+        name: "Harish Komma",
+        handle: "driesvincent",
+        imageUrl:
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Lindsay Walton",
+        handle: "lindsaywalton",
+        imageUrl:
+          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalMembers: 32,
+    lastUpdated: "March 17, 2020",
+    pinned: true,
+    bgColorClass: "bg-pink-600",
+  },
   // More projects...
 ]
 const pinnedProjects = projects.filter(project => project.pinned)

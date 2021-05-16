6dd6e1df8a4f520a0ab9bff8645db61ad8bab1cc
harish <harish.komma@onebillsoftware.com>
2021-05-16 11:20:02 +0530

    Logo

--- pages/index.js
+++ pages/index.js
@@ -229,11 +229,7 @@ export default function Example() {
       <div className="hidden lg:flex lg:flex-shrink-0">
         <div className="flex flex-col w-64 border-r border-gray-200 pt-5 pb-4 bg-gray-100">
           <div className="flex items-center flex-shrink-0 px-6">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-logo-purple-500-mark-gray-700-text.svg"
-              alt="Workflow"
-            />
+            <h1 className="text-2xl bold">Whatsnuxt...</h1>
           </div>
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="h-0 flex-1 flex flex-col overflow-y-auto">
